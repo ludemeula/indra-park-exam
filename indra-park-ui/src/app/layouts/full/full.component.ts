ed3c56ca19221f31439ef39cfbf012206196462f
ludemeula <ludemeula@gmail.com>
2019-07-06 00:46:23 -0300

    Atualizando layout

--- indra-park-ui/src/app/layouts/full/full.component.ts
+++ indra-park-ui/src/app/layouts/full/full.component.ts
@@ -2,8 +2,7 @@ import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-full-layout',
-  templateUrl: 'full.component.html',
-  styleUrls: ['full.component.css']
+  templateUrl: 'full.component.html'
 })
 export class FullComponent implements OnDestroy, AfterViewInit, OnInit {
 
